a6d10598c3769bd2d8bb8dc9cb2605b7777e3fcc
sdbds <865105819@qq.com>
2024-09-11 23:07:35 +0800

    update

--- library/flux_train_utils.py
+++ library/flux_train_utils.py
@@ -412,7 +412,6 @@ def get_noisy_model_input_and_timesteps(
         logits_norm = torch.randn(bsz, device=device)
         logits_norm = logits_norm * args.sigmoid_scale  # larger scale for more uniform sampling
         timesteps = logits_norm.sigmoid()
-        x1, x2 = args.min_bucket_reso or min(args.resolution), args.max_bucket_reso or max(args.resolution)
         mu = get_lin_function(
             y1=0.5,
             y2=1.15,
